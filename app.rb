2be57e8152b87c9b71243df6b206504c40a1b3e2
jgorman26 <156133160+jgorman26@users.noreply.github.com>
2025-01-28 22:18:36 +0000

    Commit

--- app.rb
+++ app.rb
@@ -1,9 +1,40 @@
+=begin
 require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get ("/rock") do
+end
+
+get ("/paper") do
+end
+
+get ("/scissors") do
+end
+=end
+
+require "sinatra"
+require "sinatra/reloader"
+
+get("/") do
+  erb(:rules)
+end
+
+get("/rock") do
+  1.times do
+    @move = rand(1..3)
+    if @move == 1
+      @move = "rock"
+    elsif @move == 2
+      @move = "paper"
+    else
+      @move = "scissors"
+    end
+  erb(:rock)
 end
