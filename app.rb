34ce385340598a3bb1bb80466f35d4c1d7a6ae85
jgorman26 <156133160+jgorman26@users.noreply.github.com>
2025-01-28 23:10:38 +0000

    Final push

--- app.rb
+++ app.rb
@@ -1,36 +1,57 @@
-=begin
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+def game(user, comp)
+  if user == comp
+    return "We tied!"
+  elsif (user == "rock" && comp == "scissors") ||
+        (user == "paper" && comp == "rock") ||
+        (user == "scissors" && comp == "paper")
+    return "We won!"
+  else
+    return "We lost!"
+  end
 end
-
-get ("/rock") do
-end
-
-get ("/paper") do
-end
-
-get ("/scissors") do
-end
-=end
-
-require "sinatra"
-require "sinatra/reloader"
 
 get("/") do
   erb(:rules)
 end
 
 get("/rock") do
+  move = rand(1..3)
+  if move == 1
+    @move = "rock"
+  elsif move == 2
+    @move = "paper"
+  elsif move == 3
+    @move = "scissors"
+  end
+  @result = game("rock", @move)
+  erb(:rock)
 end
 
 get("/paper") do
+  move = rand(1..3)
+  if move == 1
+    @move = "rock"
+  elsif move == 2
+    @move = "paper"
+  elsif move == 3
+    @move = "scissors"
+  end
+  @result = game("paper", @move)
+  erb(:paper)
 end
 
 get("/scissors") do
+  move = rand(1..3)
+  if move == 1
+    @move = "rock"
+  elsif move == 2
+    @move = "paper"
+  elsif move == 3
+    @move = "scissors"
+  end
+  @result = game("scissors", @move)
+  erb(:scissors)
 end
